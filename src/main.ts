156dce67e978d18815f1872ddc7eb217df152f98
Meruem117 <1572677319@qq.com>
2023-01-23 19:48:41 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+import './style.css'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
---
+++ src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+
+import Vue from 'vue'
+
+declare module '*.vue' {
+    export default Vue
+}
